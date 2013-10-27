c84528bc6d011df70feb78efc4485c07e21469c3
Maksym Tkachenko <tkachenko_m_a@hotmail.com>
2013-10-27 19:23:43 +0200

    changed appoptions

--- unusedCssFinder/Models/CommandLine/AppOptions.cs
+++ unusedCssFinder/Models/CommandLine/AppOptions.cs
@@ -7,16 +7,10 @@ namespace unusedCssFinder.Models.CommandLine
 {
     public class AppOptions
     {
-        [OptionArray('i', "input", Required = true, HelpText = "Input - valid urls or existing locally html file.")]
+        [OptionArray('i', "input", Required = true, HelpText = "Input - valid urls, not separated by comma.")]
         public string[] Input { get; set; }
 
         public List<Uri> ParsedUris { get; set; }
-            
-        [Option('d', "depth", Required = false, DefaultValue = 0, HelpText = "Page analyzing depth. 0 - only specified pages, 1 - specified page and children and so on.")]
-        public int SearchDepth { get; set; }
-
-        [Option('p', "maxPagesCount", Required = false, DefaultValue = 0, HelpText = "Maximum number of pages to analyze. 0 means only specified pages.")]
-        public int MaxPagesCount { get; set; }
 
         [Option('f', "outputFile", Required = true, HelpText = "Location of html file with results of analyzing.")]
         public string OutputFile { get; set; }
@@ -29,17 +23,17 @@ namespace unusedCssFinder.Models.CommandLine
             return defaultHelp.ToString() + @"
 Examples:
 
-Find all unused css selectors of http://uawebchallenge.com/ with it's children, but maximum 5 pages will be analyzed. Writes results to c:\results.html:
+Find all unused css selectors always overriden css selectors and directives of http://uawebchallenge.com/. Writes results to c:\results.html:
 
-unusedCssFinder -i http://uawebchallenge.com/ -d 1 -p 5 -f c:\results.html
+unusedCssFinder -i http://uawebchallenge.com/ -f c:\results.html
 
-Find all unused css selectors of only c:\test.html, c:\test2.html pages. Writes results to c:\results.html:
+Find all unused css selectors, always overriden css selectors and directives of http://uawebchallenge.com/ and http://uawebchallenge.com/partners. Writes results to c:\results.html:
 
-unusedCssFinder -i c:\test.html c:\test2.html -f c:\results.html
+unusedCssFinder -i  http://uawebchallenge.com/ http://uawebchallenge.com/partners -f c:\results.html
 
-Important note:
+Note:
 
-First of all tool analyzes specified urls. After that child urls are processed.
+If c:\results.html file existed before, it will be overwritten.
 ";
         }
 
