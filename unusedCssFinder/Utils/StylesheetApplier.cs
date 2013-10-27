c86d4731006524608a3dc32bc1bfb675dfe43918
Maksym Tkachenko <tkachenko_m_a@hotmail.com>
2013-10-27 19:37:32 +0200

    added css2xpath dll. Changed tatget framework to 4.0

--- unusedCssFinder/Utils/StylesheetApplier.cs
+++ unusedCssFinder/Utils/StylesheetApplier.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using ThirdParty.MostThingsWeb;
+using MostThingsWeb;
 using unusedCssFinder.CssData.UsageModels;
 using unusedCssFinder.Models.Html;
 
